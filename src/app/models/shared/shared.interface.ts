66285ef650924e3bdb995c38f5a568aabdff87db
anaRamirez5 <160249918+anaRamirez5@users.noreply.github.com>
2024-12-17 10:19:13 -0500

    integrate endpoint status and update filters

--- src/app/models/shared/shared.interface.ts
+++ src/app/models/shared/shared.interface.ts
@@ -3,10 +3,15 @@ export interface Filter {
   license_plate_number: string | null;
   id_servicio: string | null;
   city: string | null;
+  status?: number | null;
   page: number;
   page_size: number;
 }
 export interface City {
   municipality_id: number;
   municipality_name: string;
+}
+export interface Status {
+  status_id: number;
+  status_name: string;
 }
