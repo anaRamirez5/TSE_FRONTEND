3844380a72f27a514151b0abf587d6c91de90dcf
anaRamirez5 <160249918+anaRamirez5@users.noreply.github.com>
2025-02-26 18:06:17 -0500

    fix belongs route

--- src/app/components/private/driver/assign-per-day/accordion-asign-driver/accordion-asign-driver.component.ts
+++ src/app/components/private/driver/assign-per-day/accordion-asign-driver/accordion-asign-driver.component.ts
@@ -27,6 +27,7 @@ export class AccordionAsignDriverComponent implements OnInit {
     private driverService: DriverService
   ) {}
   ngOnInit() {
+    console.log(this.tab());
     this.license_plate_number =
       sessionStorage.getItem(SessionStorageItems.CAR_ID) ?? '';
     this.handleButtons = this.data().estado_servicio === 'confirmado';
